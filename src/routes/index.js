50942eebccc01f478d77737a6c7b810c5d2d0f96
Jody Septiawan <jody@192.168.1.17>
2021-09-29 09:39:30 +0700

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
 // import controller here
+const { addUser } = require('../controllers/user');
 
 // Route
 // add route here
+router.post('/user', addUser);
 
-
-module.exports = router
+module.exports = router;
