d2157ee0656646b63fbfeb29dcec774d2b8ec611
Jody Septiawan <jody@192.168.1.17>
2021-09-29 11:12:41 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,35 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // Controller
-const { addUsers, getUsers, getUser, updateUser, deleteUser } = require("../controllers/user");
+const {
+  addUsers,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/user');
 // import controller function here
+const { addProduct, getProducts } = require('../controllers/product');
+
+const {
+  addTransaction,
+  getTransactions,
+} = require('../controllers/transaction');
 
 // Route
-router.post("/user", addUsers);
-router.get("/users", getUsers);
-router.get("/user/:id", getUser);
-router.patch("/user/:id", updateUser);
-router.delete("/user/:id", deleteUser);
+router.post('/user', addUsers);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.patch('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 
 // add route here
+router.post('/product', addProduct);
+router.get('/products', getProducts);
+
+router.post('/transaction', addTransaction);
+router.get('/transactions', getTransactions);
 
 module.exports = router;
